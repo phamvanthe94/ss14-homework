a6a4afef920f4edb3f90ec5a839788a1c630b5b6
phamvanthe94 <mihthe05121994@gmail.com>
2025-08-14 11:42:10 +0900

    Nộp bài tập SS14_ex02

---
+++ SS14_ex02.js
@@ -0,0 +1,11 @@
+let car = {
+  brand: `Toyota`,
+  model: `Corolla`,
+  year: 2020,
+};
+// car.color = `red`; --> DOT NOTATION
+
+car[`color`] = `red`; // -->SQUARE BRACKETS
+
+car.year = 2022;
+console.log(car);
